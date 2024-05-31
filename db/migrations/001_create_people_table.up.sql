b374b5620674aebdb66f406d413de5f83238f6e6
elenakolykhalova <94307455+elenakolykhalova@users.noreply.github.com>
2024-05-31 17:14:13 +0300

    add migration

---
+++ db/migrations/001_create_people_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE people (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(50),
+                        surname VARCHAR(50),
+                        patronymic VARCHAR(50),
+                        age INT,
+                        gender VARCHAR(10),
+                        nationality VARCHAR(50)
+);
